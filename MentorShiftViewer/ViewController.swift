71e5dbafc14dd8148823b17b648523a7971a337a
TakeumiYamamura <yama1223xxx@gmail.com>
2015-12-26 12:06:19 +0900

    initial commit

---
+++ MentorShiftViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MentorShiftViewer
+//
+//  Created by TakefumiYamamura on 2015/12/26.
+//  Copyright © 2015年 div. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
